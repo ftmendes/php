d93f9a248de8765641b11b127adc187d341876ce
Felipe Mendes <felipe.torres.it@gmail.com>
2017-10-12 13:59:26 -0300

    alteracoes feitas

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Olá mundo 456!!!";
 
-	echo "Olá Udemy";
-=======
-  echo "Olá mundo 123!!!";
-
-  echo "Olá Felipe!!!
->>>>>>> 3816cf27693ebc32b47f887e5cb8853ba633333e
+	echo "Olá Felipe";
 
 ?>
