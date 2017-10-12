228e98222ed43bf25701d13b4a0c53e5df49f591
ftmendes <32723580+ftmendes@users.noreply.github.com>
2017-10-12 13:55:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Olá mundo 456!!!";
 
 	echo "Olá Udemy";
-=======
-  echo "Olá mundo 123!!!";
-
-  echo "Olá Felipe!!!
->>>>>>> 3816cf27693ebc32b47f887e5cb8853ba633333e
 
 ?>
