1688b72c26213c1da25fb1a3ffa978180087d72c
Felipe Mendes <felipe.torres.it@gmail.com>
2017-10-12 14:02:15 -0300

    adicionei outro echo

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-	echo "Olá mundo 456!!!";
+	echo "Olá mundo 123456!!!";
 
-	echo "Olá Felipe";
-=======
-	echo "Olá mundo 123!!!";
+	echo "Olá Felipe e Udemy!";
 
-	echo "Olá Udemy!";
->>>>>>> 3f0456a4bd2dbd30cfc9819d5a9de5c2314a1a66
+	echo "Git é foda"
 
 ?>
